00da8c6aa6140b3e6d529fce4939a1c321a65abe
Kiran-kumarkottali <121659688+Kiran-kumarkottali@users.noreply.github.com>
2024-09-01 12:47:43 +0530

    Added files from local

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0044aa72e124cf0d2356e0eeb778acd9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
